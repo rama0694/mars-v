694563fdea6c876ed164099aa1259e68d30661cc
Venkatesh  Naik <vnaik@tekion.com>
2023-02-26 12:11:08 +0530

    Added ui project

---
+++ pramaanyasetu/pramaanyasetu-ui/src/App.js
@@ -0,0 +1,19 @@
+import Sample from './components/sample/Sample';
+// import logo from './logo.svg';
+// import './App.css';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="child">venki</div>
+          <Sample />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ pramaanyasetu/pramaanyasetu-ui/src/components/sample/Sample.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './sample.module.scss';
+
+const Sample = () => <div className={styles.marsV}>Marsv</div>
+
+
+export default Sample;
