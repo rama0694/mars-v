47e43f68d7f01118826ac8440d0385b96cbce08e
Venkatesh  Naik <vnaik@tekion.com>
2023-02-26 20:23:05 +0530

    mobile enahancements

--- pramaanyasetu/pramaanyasetu-ui/src/pages/signIn/SignIn.js
+++ pramaanyasetu/pramaanyasetu-ui/src/pages/signIn/SignIn.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grid, Typography, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { Container, Grid, Typography, RadioGroup, FormControlLabel, Radio, Box } from '@mui/material';
 import GoogleButton from 'react-google-button';
 import { useNavigate } from 'react-router-dom';
 import singinImage from '../../images/login.svg';
@@ -29,7 +29,9 @@ const SignIn = props => {
       <Grid container spacing={2}>
         <Grid item md={6} sm={12} xs={12} className="border-right">
           <div className="display-flex flex-column align-items-center justify-content-center h-100 p-2">
-            <img src={singinImage} alt="" className="my-4" />
+            <Box className="w-100" sx={{ height: { sm: 320, xs: 320, md: '100%', lg: '100%' } }}>
+              <img src={singinImage} alt="" className="my-4" />
+            </Box>
           </div>
         </Grid>
         <Grid item md={6} sm={12} xs={12}>
