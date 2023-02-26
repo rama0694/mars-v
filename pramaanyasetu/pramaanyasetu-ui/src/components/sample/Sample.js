2e5d6f456e62837396fc3ea3afeabdd61987f44e
Venkatesh  Naik <vnaik@tekion.com>
2023-02-26 12:40:30 +0530

    ui setup done

--- pramaanyasetu/pramaanyasetu-ui/src/components/sample/Sample.js
+++ pramaanyasetu/pramaanyasetu-ui/src/components/sample/Sample.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import styles from './sample.module.scss';
 
-const Sample = () => <div className={styles.marsV}>Marsv</div>
+const Sample = () => <div className={styles.marsV}>
+  <Button onClick={() => console.log("hello")}>Click me</Button>
+</div>
 
 
 export default Sample;
